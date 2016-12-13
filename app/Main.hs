0333d710518a07947db7e4ae621a9f3818c1891d
Koray Al <admin@koray.al>
2016-12-14 01:15:33 +0300

    Completed Day 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import qualified Day1 as D1
+import System.Environment (getArgs)
 
 main :: IO ()
-main = D1.someFunc
+main = do
+  args <- getArgs
+  case args !! 0 of
+    "1a" -> D1.solveFirst
+    "1b" -> D1.solveSecond
+    _    -> putStrLn "Not Yet"
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,6 +1,99 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day1
-    ( someFunc
+    ( solveFirst
+    , solveSecond
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Applicative ((<*>),
+                            (*>),
+                            (<$>),
+                            (<|>),
+                            pure)
+
+import qualified Data.Attoparsec.Text as A
+import qualified Data.Attoparsec.Combinator as AC
+import Data.Attoparsec.Text (Parser)
+import Data.Text (Text)
+import qualified Data.Text.IO as I
+
+pos :: Location
+pos = Location North 0 0
+
+solveFirst :: IO ()
+solveFirst = do
+  l <- I.getLine
+  dse <- pure $ A.parseOnly directions l
+  case dse of
+    Left _ -> putStrLn "Invalid Input"
+    Right ds -> let (Location f x y) = applyDirections pos ds
+               in putStrLn $ show $ (abs x) + (abs y)
+
+solveSecond :: IO ()
+solveSecond = do
+  l <- I.getLine
+  dse <- pure $ A.parseOnly directions l
+  case dse of
+    Left _ -> putStrLn "Invalid Input"
+    Right ds -> case repeatedLocation (logDirections pos ds) of
+      Nothing -> putStrLn "Smart people plan their route first"
+      Just (Location f x y) -> putStrLn $ show $ (abs x) + (abs y)
+
+data Direction = DirectionLeft Int | DirectionRight Int | DirectionForward Int deriving (Eq, Show)
+
+-- INPUT PARSERS
+direction :: Parser Direction
+direction = (left <*> A.decimal) <|>
+            (right <*> A.decimal)
+  where left = A.char 'L' *> pure DirectionLeft
+        right = A.char 'R' *> pure DirectionRight
+
+directions :: Parser [Direction]
+directions = do
+  ds <- direction `AC.sepBy` (A.string ", ")
+  return ds
+
+data Facing = North | East | South | West deriving (Eq, Show, Enum, Bounded)
+
+data Location = Location Facing Int Int deriving (Eq, Show)
+
+prev :: (Eq a, Enum a, Bounded a) => a -> a
+prev e | e == minBound = maxBound
+       | otherwise = pred e
+
+next :: (Eq a, Enum a, Bounded a) => a -> a
+next e | e == maxBound = minBound
+       | otherwise = succ e
+
+moveDirection :: Location -> Direction -> Location
+moveDirection (Location f x y) d =
+  let newLocation r n = case r of
+        North -> Location North x (y + n)
+        East -> Location East (x + n) y
+        South -> Location South x (y - n)
+        West -> Location West (x - n) y
+  in case d of
+    DirectionLeft n -> newLocation (prev f) n
+    DirectionRight n -> newLocation (next f) n
+    DirectionForward n -> newLocation f n
+
+applyDirections :: Location -> [Direction] -> Location
+applyDirections = foldl moveDirection
+
+logDirections :: Location -> [Direction] -> [Location]
+logDirections l d = scanl moveDirection l (concatMap expandDirection d)
+
+expandDirection :: Direction -> [Direction]
+expandDirection (DirectionLeft x)
+  | x == 1 = [DirectionLeft 1]
+  | otherwise = (DirectionLeft 1) : (replicate (x - 1) (DirectionForward 1))
+expandDirection (DirectionRight x)
+  | x == 1 = [DirectionRight 1]
+  | otherwise = (DirectionRight 1) : (replicate (x - 1) (DirectionForward 1))
+
+repeatedLocation :: [Location] -> Maybe Location
+repeatedLocation [] = Nothing
+repeatedLocation ((Location _ xx yy):xs) = repeatedLocation' [(xx, yy)] xs
+  where repeatedLocation' old [] = Nothing
+        repeatedLocation' old (x@(Location _ xx2 yy2):xs) = if elem (xx2, yy2) old
+                                       then Just x
+                                       else repeatedLocation' ((xx2, yy2):old) xs
